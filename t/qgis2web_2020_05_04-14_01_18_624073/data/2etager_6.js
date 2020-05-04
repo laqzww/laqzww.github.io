04bf2a962426e8bbd174a2b446ab26daa11dee4d
laqzww <laqzw@hotmail.com>
2020-05-04 14:50:05 +0200

    kort

---
+++ t/qgis2web_2020_05_04-14_01_18_624073/data/2etager_6.js
@@ -0,0 +1,8 @@
+var json_2etager_6 = {
+"type": "FeatureCollection",
+"name": "2etager_6",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+
+]
+}
