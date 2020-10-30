4c1e92d1b87259fb7f7ea86ce8c5660cf296e9e9
laqzww <laqzw@hotmail.com>
2020-10-30 03:08:46 +0100

    SD
    SD

--- t/qgis2web_2020_05_04-14_01_18_624073/data/2etager_6.js
+++
@@ -1,8 +0,0 @@
-var json_2etager_6 = {
-"type": "FeatureCollection",
-"name": "2etager_6",
-"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
-"features": [
-
-]
-}
